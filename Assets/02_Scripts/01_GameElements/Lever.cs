d6a54fe844c3f1ad39a3e75b47dfe40531329e22
dorF <nemoz.burkhalter@gmail.com>
2022-03-15 17:53:19 +0100

    Line to linked elements

--- Assets/02_Scripts/01_GameElements/Lever.cs
+++ Assets/02_Scripts/01_GameElements/Lever.cs
@@ -4,9 +4,34 @@ using UnityEngine;
 
 public class Lever : Interactable
 {
+    //Wether this is activated or not
     [SerializeField] protected bool _isOn = false;
 
+    [Tooltip("Elements that this will activate")]
     [SerializeField] protected List<Activatable> _linked = new List<Activatable>();
+
+    [Tooltip("Line object used to draw lines to the linked objects")]
+    [SerializeField] GameObject _line;
+    List<GameObject> _lines = new List<GameObject>();
+
+    private void Start()
+    {
+        //Draw lines to the linked object
+        foreach (GameObject lineObject in _lines)
+        {
+            Destroy(lineObject);
+        }
+        _lines = new List<GameObject>();
+        foreach (Activatable activatable in _linked)
+        {
+            GameObject lineObject = Instantiate(_line, transform);
+            lineObject.GetComponent<LineRenderer>().SetPositions(
+                new Vector3[] { transform.position, activatable.transform.position });
+            lineObject.GetComponent<LineRenderer>().useWorldSpace = true;
+
+            _lines.Add(lineObject);
+        }
+    }
 
     public override bool Interact()
     {
