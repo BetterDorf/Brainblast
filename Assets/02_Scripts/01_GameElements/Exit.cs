484bb1744645ddd8202eb0dceafa5e2ba1e99131
dorF <nemoz.burkhalter@gmail.com>
2022-04-12 10:12:14 +0200

    Changed visual and added anim for door

--- Assets/02_Scripts/01_GameElements/Exit.cs
+++ Assets/02_Scripts/01_GameElements/Exit.cs
@@ -1,9 +1,27 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Exit : MonoBehaviour
 {
+    [SerializeField] EventScriptableObject _playerMove;
+
+    Animator _animator;
+    bool _isOpen = false;
+
+    private void Start()
+    {
+        _playerMove.OnEventTriggered += CheckIfPlayerIsClose;
+
+        _animator = GetComponentInChildren<Animator>();
+    }
+
+    private void OnDestroy()
+    {
+        _playerMove.OnEventTriggered -= CheckIfPlayerIsClose;
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Player player;
@@ -12,5 +30,35 @@ public class Exit : MonoBehaviour
         {
             player.ExitLevel();
         }
+    }
+
+    void CheckIfPlayerIsClose()
+    {
+        Collider2D[] collider2Ds = Physics2D.OverlapBoxAll(transform.position, Vector2.one * 2, 0.0f, LayerMask.GetMask("Default"));
+
+        //Check if player is within the found items
+        if(Array.Exists(collider2Ds, (Collider2D col) => { return col.GetComponent<Player>() != null; }))
+        {
+            //Open the door
+            OpenOrClose(true);
+        }
+        else if (_isOpen)
+        {
+            //Close the door if player isn't near and door was open
+            OpenOrClose(false);
+        }
+    }
+    
+    /// <summary>
+    /// Opens or closes the door (purely visual)
+    /// </summary>
+    /// <param name="open">Wether the door should be open or closed</param>
+    void OpenOrClose(bool open)
+    {
+        //Switch the door's status
+        _isOpen = open;
+
+        //Update the animator
+        _animator.SetBool("isOpen", _isOpen);
     }
 }
