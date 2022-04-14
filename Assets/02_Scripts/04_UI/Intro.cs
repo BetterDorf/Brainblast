279a74b14f6bb023c411794c78b644442392fb1b
dorF <nemoz.burkhalter@gmail.com>
2022-04-15 01:54:44 +0200

    Intro and victory incorporated in the main game

--- Assets/02_Scripts/04_UI/Intro.cs
+++ Assets/02_Scripts/04_UI/Intro.cs
@@ -1,17 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Intro : MonoBehaviour
 {
     [SerializeField] GameObject _backgroundSprite;
     [SerializeField] int _number;
 
+    [SerializeField] bool _quitOnKeyDown = false;
+
     private void Start()
     {
         for (int i = 0; i < _number; i++)
         {
             Instantiate(_backgroundSprite);
+        }
+    }
+
+    private void Update()
+    {
+        //Exit this screen when anything is pressed
+        if (Input.anyKeyDown)
+        {
+            //Quit the game if we're the last scene
+            if (_quitOnKeyDown)
+            {
+                Application.Quit();
+            }
+            else
+            {
+                //load the next scene
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
         }
     }
 }
